ef1337f0bc6b2d9bbbddf90c4616f29640e74bf7
CharlesSampson44 <CharlesSampson44@outlook.com>
2022-03-31 00:55:43 +0800

    fix bn

--- lib/abi_gen/wyvern_atomicizer.d.ts
+++ lib/abi_gen/wyvern_atomicizer.d.ts
@@ -3,14 +3,13 @@
  * Templates can be found at https://github.com/0xProject/0x.js/tree/development/packages/abi-gen-templates.
  */
 import { TxData } from '../types';
-import BN from 'bn.js';
 import { BaseContract } from './base_contract';
 import { Contract } from "web3-eth-contract";
 export declare class WyvernAtomicizerContract extends BaseContract {
     atomicize: {
-        sendTransactionAsync(addrs_0: string[], values_1: BN[], calldataLengths_2: BN[], calldatas_3: string, txData?: Partial<TxData>): Promise<string>;
-        estimateGasAsync(addrs_0: string[], values_1: BN[], calldataLengths_2: BN[], calldatas_3: string, txData?: Partial<TxData>): Promise<number>;
-        getABIEncodedTransactionData(addrs_0: string[], values_1: BN[], calldataLengths_2: BN[], calldatas_3: string): string;
+        sendTransactionAsync(addrs_0: string[], values_1: string[], calldataLengths_2: string[], calldatas_3: string, txData?: Partial<TxData>): Promise<string>;
+        estimateGasAsync(addrs_0: string[], values_1: string[], calldataLengths_2: string[], calldatas_3: string, txData?: Partial<TxData>): Promise<number>;
+        getABIEncodedTransactionData(addrs_0: string[], values_1: string[], calldataLengths_2: string[], calldatas_3: string): string;
     };
     constructor(contract: Contract, defaults: Partial<TxData>);
 }
--- lib/abi_gen/wyvern_exchange.d.ts
+++ lib/abi_gen/wyvern_exchange.d.ts
@@ -3,7 +3,6 @@
  * Templates can be found at https://github.com/0xProject/0x.js/tree/development/packages/abi-gen-templates.
  */
 import { TxData } from '../types';
-import BN from 'bn.js';
 import { BaseContract } from './base_contract';
 import { Contract } from "web3-eth-contract";
 export declare class WyvernExchangeContract extends BaseContract {
@@ -20,14 +19,14 @@ export declare class WyvernExchangeContract extends BaseContract {
         callAsync(target_0: string, calldata_1: string, extradata_2: string, txData?: Partial<TxData>): Promise<boolean>;
     };
     changeMinimumMakerProtocolFee: {
-        sendTransactionAsync(newMinimumMakerProtocolFee_0: BN, txData?: Partial<TxData>): Promise<string>;
-        estimateGasAsync(newMinimumMakerProtocolFee_0: BN, txData?: Partial<TxData>): Promise<number>;
-        getABIEncodedTransactionData(newMinimumMakerProtocolFee_0: BN): string;
+        sendTransactionAsync(newMinimumMakerProtocolFee_0: string, txData?: Partial<TxData>): Promise<string>;
+        estimateGasAsync(newMinimumMakerProtocolFee_0: string, txData?: Partial<TxData>): Promise<number>;
+        getABIEncodedTransactionData(newMinimumMakerProtocolFee_0: string): string;
     };
     changeMinimumTakerProtocolFee: {
-        sendTransactionAsync(newMinimumTakerProtocolFee_0: BN, txData?: Partial<TxData>): Promise<string>;
-        estimateGasAsync(newMinimumTakerProtocolFee_0: BN, txData?: Partial<TxData>): Promise<number>;
-        getABIEncodedTransactionData(newMinimumTakerProtocolFee_0: BN): string;
+        sendTransactionAsync(newMinimumTakerProtocolFee_0: string, txData?: Partial<TxData>): Promise<string>;
+        estimateGasAsync(newMinimumTakerProtocolFee_0: string, txData?: Partial<TxData>): Promise<number>;
+        getABIEncodedTransactionData(newMinimumTakerProtocolFee_0: string): string;
     };
     guardedArrayReplace: {
         callAsync(array_0: string, desired_1: string, mask_2: string, txData?: Partial<TxData>): Promise<string>;
@@ -38,7 +37,7 @@ export declare class WyvernExchangeContract extends BaseContract {
         getABIEncodedTransactionData(newAuctionBroker_0: string): string;
     };
     minimumTakerProtocolFee: {
-        callAsync(txData?: Partial<TxData>): Promise<BN>;
+        callAsync(txData?: Partial<TxData>): Promise<string>;
     };
     codename: {
         callAsync(txData?: Partial<TxData>): Promise<string>;
@@ -50,7 +49,7 @@ export declare class WyvernExchangeContract extends BaseContract {
         callAsync(arrToCopy_0: string, txData?: Partial<TxData>): Promise<string>;
     };
     calculateCurrentPrice_: {
-        callAsync(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, txData?: Partial<TxData>): Promise<BN>;
+        callAsync(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, txData?: Partial<TxData>): Promise<string>;
     };
     changeProtocolFeeRecipient: {
         sendTransactionAsync(newProtocolFeeRecipient_0: string, txData?: Partial<TxData>): Promise<string>;
@@ -64,10 +63,10 @@ export declare class WyvernExchangeContract extends BaseContract {
         callAsync(buyCalldata_0: string, buyReplacementPattern_1: string, sellCalldata_2: string, sellReplacementPattern_3: string, txData?: Partial<TxData>): Promise<boolean>;
     };
     validateOrder_: {
-        callAsync(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, v_9: number | BN, r_10: string, s_11: string, txData?: Partial<TxData>): Promise<boolean>;
+        callAsync(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, v_9: number | string, r_10: string, s_11: string, txData?: Partial<TxData>): Promise<boolean>;
     };
     calculateFinalPrice: {
-        callAsync(side_0: number | BN, saleKind_1: number | BN, basePrice_2: BN, extra_3: BN, listingTime_4: BN, expirationTime_5: BN, txData?: Partial<TxData>): Promise<BN>;
+        callAsync(side_0: number | string, saleKind_1: number | string, basePrice_2: string, extra_3: string, listingTime_4: string, expirationTime_5: string, txData?: Partial<TxData>): Promise<string>;
     };
     protocolFeeRecipient: {
         callAsync(txData?: Partial<TxData>): Promise<string>;
@@ -78,24 +77,24 @@ export declare class WyvernExchangeContract extends BaseContract {
         getABIEncodedTransactionData(): string;
     };
     hashOrder_: {
-        callAsync(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, txData?: Partial<TxData>): Promise<string>;
+        callAsync(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, txData?: Partial<TxData>): Promise<string>;
     };
     ordersCanMatch_: {
-        callAsync(addrs_0: string[], uints_1: BN[], feeMethodsSidesKindsHowToCalls_2: Array<number | BN>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, txData?: Partial<TxData>): Promise<boolean>;
+        callAsync(addrs_0: string[], uints_1: string[], feeMethodsSidesKindsHowToCalls_2: Array<number | string>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, txData?: Partial<TxData>): Promise<boolean>;
     };
     approveOrder_: {
-        sendTransactionAsync(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, orderbookInclusionDesired_9: boolean, txData?: Partial<TxData>): Promise<string>;
-        estimateGasAsync(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, orderbookInclusionDesired_9: boolean, txData?: Partial<TxData>): Promise<number>;
-        getABIEncodedTransactionData(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, orderbookInclusionDesired_9: boolean): string;
+        sendTransactionAsync(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, orderbookInclusionDesired_9: boolean, txData?: Partial<TxData>): Promise<string>;
+        estimateGasAsync(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, orderbookInclusionDesired_9: boolean, txData?: Partial<TxData>): Promise<number>;
+        getABIEncodedTransactionData(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, orderbookInclusionDesired_9: boolean): string;
     };
     registry: {
         callAsync(txData?: Partial<TxData>): Promise<string>;
     };
     minimumMakerProtocolFee: {
-        callAsync(txData?: Partial<TxData>): Promise<BN>;
+        callAsync(txData?: Partial<TxData>): Promise<string>;
     };
     hashToSign_: {
-        callAsync(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, txData?: Partial<TxData>): Promise<string>;
+        callAsync(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, txData?: Partial<TxData>): Promise<string>;
     };
     cancelledOrFinalized: {
         callAsync(index_0_0: string, txData?: Partial<TxData>): Promise<boolean>;
@@ -104,23 +103,23 @@ export declare class WyvernExchangeContract extends BaseContract {
         callAsync(txData?: Partial<TxData>): Promise<string>;
     };
     cancelOrder_: {
-        sendTransactionAsync(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, v_9: number | BN, r_10: string, s_11: string, txData?: Partial<TxData>): Promise<string>;
-        estimateGasAsync(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, v_9: number | BN, r_10: string, s_11: string, txData?: Partial<TxData>): Promise<number>;
-        getABIEncodedTransactionData(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, v_9: number | BN, r_10: string, s_11: string): string;
+        sendTransactionAsync(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, v_9: number | string, r_10: string, s_11: string, txData?: Partial<TxData>): Promise<string>;
+        estimateGasAsync(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, v_9: number | string, r_10: string, s_11: string, txData?: Partial<TxData>): Promise<number>;
+        getABIEncodedTransactionData(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, v_9: number | string, r_10: string, s_11: string): string;
     };
     atomicMatch_: {
-        sendTransactionAsync(addrs_0: string[], uints_1: BN[], feeMethodsSidesKindsHowToCalls_2: Array<number | BN>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, vs_9: Array<number | BN>, rssMetadata_10: string[], txData?: Partial<TxData>): Promise<string>;
-        estimateGasAsync(addrs_0: string[], uints_1: BN[], feeMethodsSidesKindsHowToCalls_2: Array<number | BN>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, vs_9: Array<number | BN>, rssMetadata_10: string[], txData?: Partial<TxData>): Promise<number>;
-        getABIEncodedTransactionData(addrs_0: string[], uints_1: BN[], feeMethodsSidesKindsHowToCalls_2: Array<number | BN>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, vs_9: Array<number | BN>, rssMetadata_10: string[]): string;
+        sendTransactionAsync(addrs_0: string[], uints_1: string[], feeMethodsSidesKindsHowToCalls_2: Array<number | string>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, vs_9: Array<number | string>, rssMetadata_10: string[], txData?: Partial<TxData>): Promise<string>;
+        estimateGasAsync(addrs_0: string[], uints_1: string[], feeMethodsSidesKindsHowToCalls_2: Array<number | string>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, vs_9: Array<number | string>, rssMetadata_10: string[], txData?: Partial<TxData>): Promise<number>;
+        getABIEncodedTransactionData(addrs_0: string[], uints_1: string[], feeMethodsSidesKindsHowToCalls_2: Array<number | string>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, vs_9: Array<number | string>, rssMetadata_10: string[]): string;
     };
     validateOrderParameters_: {
-        callAsync(addrs_0: string[], uints_1: BN[], feeMethod_2: number | BN, side_3: number | BN, saleKind_4: number | BN, howToCall_5: number | BN, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, txData?: Partial<TxData>): Promise<boolean>;
+        callAsync(addrs_0: string[], uints_1: string[], feeMethod_2: number | string, side_3: number | string, saleKind_4: number | string, howToCall_5: number | string, calldata_6: string, replacementPattern_7: string, staticExtradata_8: string, txData?: Partial<TxData>): Promise<boolean>;
     };
     INVERSE_BASIS_POINT: {
-        callAsync(txData?: Partial<TxData>): Promise<BN>;
+        callAsync(txData?: Partial<TxData>): Promise<string>;
     };
     calculateMatchPrice_: {
-        callAsync(addrs_0: string[], uints_1: BN[], feeMethodsSidesKindsHowToCalls_2: Array<number | BN>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, txData?: Partial<TxData>): Promise<BN>;
+        callAsync(addrs_0: string[], uints_1: string[], feeMethodsSidesKindsHowToCalls_2: Array<number | string>, calldataBuy_3: string, calldataSell_4: string, replacementPatternBuy_5: string, replacementPatternSell_6: string, staticExtradataBuy_7: string, staticExtradataSell_8: string, txData?: Partial<TxData>): Promise<string>;
     };
     approvedOrders: {
         callAsync(index_0_0: string, txData?: Partial<TxData>): Promise<boolean>;
